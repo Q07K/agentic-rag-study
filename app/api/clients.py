5f0d572b6532920bffc07e7725565f0020e14c76
Q07K <kgh0730@gmail.com>
2025-06-20 22:11:49 +0900

    WIP: Implement create_client endpoint and enhance dependency injection for database session

--- app/api/clients.py
+++ app/api/clients.py
@@ -1,4 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from app import crud
+from app.db.sqlite_session import get_db
 
 router = APIRouter(
     prefix="/v1/clients",
@@ -6,21 +10,37 @@ router = APIRouter(
 )
 
 
-@router.get("/")
-def create_client():
-    pass
+@router.post(path="/")
+def create_client(db: Session = Depends(dependency=get_db)):
+    crud.clients.create_client(
+        db=db,
+        client_id="example_api_key1",
+        user="example_user",
+        encrypted_uri="example_encrypted_uri",
+        encrypted_token="example_encrypted_token",
+    )
+    return {"message": "Client created successfully"}
 
 
-@router.get("/{client_id}")
-def get_client():
-    pass
+@router.get(path="/{client_id}")
+def get_client(
+    client_id: str,
+    db: Session = Depends(dependency=get_db),
+):
+    return crud.clients.get_client_by_client_id(db=db, client_id=client_id)
 
 
-@router.put("/{client_id}")
+@router.put(path="/{client_id}")
 def update_client():
     pass
 
 
-@router.delete("/{client_id}")
-def delete_client():
-    pass
+@router.delete(path="/{client_id}")
+def delete_client(
+    client_id: str,
+    db: Session = Depends(dependency=get_db),
+):
+    return crud.clients.delete_client_by_client_id(
+        db=db,
+        client_id=client_id,
+    )
