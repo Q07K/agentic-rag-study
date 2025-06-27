db4b855cff52bfa45dd3a249db1fbd4c73054616
Q07K <kgh0730@gmail.com>
2025-06-28 02:24:48 +0900

    Refactor: Update references from MilvusKey to ClientKey

--- app/models/client_keys.py
+++ app/models/client_keys.py
@@ -3,8 +3,8 @@ from sqlalchemy import Column, Integer, String
 from app.db.sqlite_base import Base
 
 
-class MilvusKey(Base):
-    __tablename__ = "milvus_keys"
+class ClientKey(Base):
+    __tablename__ = "client_keys"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     client_id = Column(String, unique=True, nullable=False, index=True)
