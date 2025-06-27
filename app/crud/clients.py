092d851ea1444a2d869164682f98fd91b91981e0
Q07K <kgh0730@gmail.com>
2025-06-28 02:24:00 +0900

    Refactor: Update client model references from MilvusKey to ClientKey in CRUD operations

--- app/crud/clients.py
+++ app/crud/clients.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 
-from app.models.milvus_keys import MilvusKey
+from app.models.client_keys import ClientKey
 
 
 def create_client(
@@ -9,8 +9,8 @@ def create_client(
     user: str,
     encrypted_uri: str,
     encrypted_token: str,
-) -> MilvusKey:
-    model = MilvusKey(
+) -> ClientKey:
+    model = ClientKey(
         client_id=client_id,
         user=user,
         encrypted_uri=encrypted_uri,
@@ -22,16 +22,31 @@ def create_client(
     return model
 
 
-def get_client_by_client_id(db: Session, client_id: str) -> MilvusKey | None:
-    return db.query(MilvusKey).filter(MilvusKey.client_id == client_id).first()
+def read_client_by_client_id(db: Session, client_id: str) -> ClientKey | None:
+    return db.query(ClientKey).filter(ClientKey.client_id == client_id).first()
+
+
+def update_client_by_client_id(
+    db: Session,
+    client_id: str,
+    user: str,
+    encrypted_uri: str,
+    encrypted_token: str,
+) -> ClientKey | None:
+    model = read_client_by_client_id(db=db, client_id=client_id)
+    if model:
+        model.user = user
+        model.encrypted_uri = encrypted_uri
+        model.encrypted_token = encrypted_token
+        db.commit()
+        db.refresh(instance=model)
+    return model
 
 
 def delete_client_by_client_id(
     db: Session, client_id: str
-) -> MilvusKey | None:
-    model = (
-        db.query(MilvusKey).filter(MilvusKey.client_id == client_id).first()
-    )
+) -> ClientKey | None:
+    model = read_client_by_client_id(db=db, client_id=client_id)
     if model:
         db.delete(instance=model)
         db.commit()
